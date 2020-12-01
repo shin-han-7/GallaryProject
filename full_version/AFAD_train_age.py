c42ba30eff173a8bb5a66968908a805c9b75031d
shin-han-7 <hannah880708@gmail.com>
2020-12-02 00:40:27 +0800

    create full version for train

---
+++ full_version/AFAD_train_age.py
@@ -0,0 +1,348 @@
+"""
+1130 for finish quickly
+turn num_epochs 200 to 1
+trun BATCH_SIZE 256 to 64 to 80
+
+"""
+"""
+01.args
+02.log
+03.setting globle
+04.data load
+05.init train(resnet)
+06.train(valid:save best model)
+07.test
+08.save model
+"""
+
+import os
+import time
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import argparse
+
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from PIL import Image
+
+from ResNet34 import resnet34_
+
+######################
+# 01.args
+#######################
+TRAIN_CSV_PATH = 'D:/CollageProj/2020_gallary/prepare/training_set.csv'
+TEST_CSV_PATH = 'D:/CollageProj/2020_gallary/prepare/testing_set.csv'
+VALID_CSV_PATH = 'D:/CollageProj/2020_gallary/prepare/validing_set.csv'
+IMAGE_ROOT = 'D:/DeepLearning/GAR/1202/AFAD-Full/'
+MODEL_PT_NAME = 'best_model.pt'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--cuda',type=int,default=-1)
+parser.add_argument('--seed',type=int,default=123)
+parser.add_argument('--storepath',type=str,required=True)
+parser.add_argument('--epoch',type=int,required=True,help = 'num_epochs')
+parser.add_argument('--batch',type=int,required=True , help = 'BATCH_SIZE')
+parser.add_argument('--imp_weight',type=int,default=0)
+args = parser.parse_args()
+
+if args.cuda >= 0:
+    DEVICE = torch.device("cuda:%d" % args.cuda)
+else:
+    DEVICE = torch.device("cpu")
+
+if args.seed == -1:
+    RANDOM_SEED = None
+else:
+    RANDOM_SEED = args.seed
+
+STORE_PATH = args.storepath
+if not os.path.exists(STORE_PATH):
+    os.mkdir(STORE_PATH)
+LOGFILE = os.path.join(STORE_PATH, 'training1130.log')
+TEST_PREDICTIONS = os.path.join(STORE_PATH, 'test_predictions.log')
+TEST_ALLPROBAS = os.path.join(STORE_PATH, 'test_allprobas.tensor')
+
+IMP_WEIGHT = args.imp_weight
+num_epochs = args.epoch
+BATCH_SIZE = args.batch
+AGE_NUM = 58#21
+
+######################
+# 02.Logging
+#######################
+header = []
+header.append('PyTorch Version: %s' % torch.__version__)
+header.append('CUDA device available: %s' % torch.cuda.is_available())
+header.append('Using CUDA device: %s' % DEVICE)
+header.append('Random Seed: %s' % RANDOM_SEED)
+header.append('Output Path: %s' % STORE_PATH)
+header.append('Task Importance Weight: %s' % IMP_WEIGHT)
+
+with open(LOGFILE, 'w') as f:
+    for entry in header:
+        print(entry)
+        f.write('%s\n' % entry)
+        f.flush()
+
+
+##########################
+# 03.SETTINGS
+##########################
+NUM_WORKERS = 0 
+learning_rate = 0.0005
+#num_epochs = 1#50#200
+#AGE_NUM = 58#21
+#BATCH_SIZE = 80#64
+GRAYSCALE = False
+
+df = pd.read_csv(TRAIN_CSV_PATH, index_col=0)
+ages = df['ageID'].values
+del df
+ages = torch.tensor(ages, dtype=torch.float)
+
+def task_importance_weights(label_array):
+    uniq = torch.unique(label_array)
+    num_examples = label_array.size(0)
+    m = torch.zeros(uniq.shape[0])
+    for i, t in enumerate(torch.arange(torch.min(uniq), torch.max(uniq))):
+        m_k = torch.max(torch.tensor([label_array[label_array > t].size(0), 
+                                      num_examples - label_array[label_array > t].size(0)]))
+        m[i] = torch.sqrt(m_k.float())
+
+    imp = m/torch.max(m)
+    return imp
+
+# Data-specific scheme
+if not IMP_WEIGHT:
+    imp = torch.ones(AGE_NUM-1, dtype=torch.float)
+elif IMP_WEIGHT == 1:
+    imp = task_importance_weights(ages)
+    imp = imp[0:AGE_NUM-1]
+else:
+    raise ValueError('Incorrect importance weight parameter.')
+imp = imp.to(DEVICE)
+
+
+###################
+# 04.DataLoad
+###################
+class AFADDatasetAge(Dataset):
+    """Custom Dataset for loading AFAD face images"""
+
+    def __init__(self, csv_path, img_dir, transform=None):
+
+        df = pd.read_csv(csv_path, index_col=0)
+        self.img_dir = img_dir
+        self.csv_path = csv_path
+        self.img_paths = df['path']
+        self.y = df['ageID'].values
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img = Image.open(os.path.join(self.img_dir,
+                                      self.img_paths[index]))
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        label = self.y[index]
+        levels = [1]*label + [0]*(AGE_NUM - 1 - label)
+        levels = torch.tensor(levels, dtype=torch.float32)
+
+        return img, label, levels
+
+    def __len__(self):
+        return self.y.shape[0]
+
+
+custom_transform = transforms.Compose([transforms.Resize((128, 128)),
+                                       transforms.RandomCrop((120, 120)),
+                                       transforms.ToTensor()])
+
+train_dataset = AFADDatasetAge(csv_path=TRAIN_CSV_PATH,
+                               img_dir=IMAGE_ROOT,
+                               transform=custom_transform)
+
+custom_transform2 = transforms.Compose([transforms.Resize((128, 128)),
+                                        transforms.CenterCrop((120, 120)),
+                                        transforms.ToTensor()])
+
+test_dataset = AFADDatasetAge(csv_path=TEST_CSV_PATH,
+                              img_dir=IMAGE_ROOT,
+                              transform=custom_transform2)
+
+valid_dataset = AFADDatasetAge(csv_path=VALID_CSV_PATH,
+                               img_dir=IMAGE_ROOT,
+                               transform=custom_transform2)
+
+train_loader = DataLoader(dataset=train_dataset,
+                          batch_size=BATCH_SIZE,
+                          shuffle=True,
+                          num_workers=NUM_WORKERS)
+
+test_loader = DataLoader(dataset=test_dataset,
+                         batch_size=BATCH_SIZE,
+                         shuffle=False,
+                         num_workers=NUM_WORKERS)
+
+valid_loader = DataLoader(dataset=valid_dataset,
+                          batch_size=BATCH_SIZE,
+                          shuffle=False,
+                          num_workers=NUM_WORKERS)
+
+
+###########################################
+# 05.Initialize Cost, Model, and Optimizer
+###########################################
+
+def cost_fn(logits, levels, imp):
+    val = (-torch.sum((F.logsigmoid(logits)*levels
+                      + (F.logsigmoid(logits) - logits)*(1-levels))*imp,dim=1))
+    return torch.mean(val)
+
+
+torch.manual_seed(RANDOM_SEED)
+torch.cuda.manual_seed(RANDOM_SEED)
+###05.resnet###
+model = resnet34_(AGE_NUM, GRAYSCALE)
+model.to(DEVICE)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
+
+########
+#06.training
+#########
+def compute_mae_and_mse(model, data_loader, device):
+    mae, mse, num_examples = 0, 0, 0
+    for i, (features, targets, levels) in enumerate(data_loader):
+
+        features = features.to(device)
+        targets = targets.to(device)
+
+        logits, probas = model(features)
+        predict_levels = probas > 0.5
+        predicted_labels = torch.sum(predict_levels, dim=1)
+        num_examples += targets.size(0)
+        mae += torch.sum(torch.abs(predicted_labels - targets))
+        mse += torch.sum((predicted_labels - targets)**2)
+    mae = mae.float() / num_examples
+    mse = mse.float() / num_examples
+    return mae, mse
+
+start_time = time.time()
+best_mae, best_rmse, best_epoch = 999, 999, -1
+for epoch in range(num_epochs):
+
+    model.train()
+    for batch_idx, (features, targets, levels) in enumerate(train_loader):
+
+        features = features.to(DEVICE)
+        targets = targets
+        targets = targets.to(DEVICE)
+        levels = levels.to(DEVICE)
+
+        # FORWARD AND BACK PROP
+        logits, probas = model(features)
+        cost = cost_fn(logits, levels, imp)
+        optimizer.zero_grad()
+
+        cost.backward()
+
+        # UPDATE MODEL PARAMETERS
+        optimizer.step()
+
+        # LOGGING
+        if not batch_idx % 50:
+            s = ('Epoch: %03d/%03d | Batch %04d/%04d | Cost: %.4f'
+                 % (epoch+1, num_epochs, batch_idx,len(train_dataset)//BATCH_SIZE, cost))
+            print(s)
+            with open(LOGFILE, 'a') as f:
+                f.write('%s\n' % s)
+    
+    ########### Valid ##############
+    model.eval()
+    with torch.set_grad_enabled(False):
+        valid_mae, valid_mse = compute_mae_and_mse(model, valid_loader,device=DEVICE)
+    if valid_mae < best_mae:
+        best_mae, best_rmse, best_epoch = valid_mae, torch.sqrt(valid_mse), epoch
+        ########## SAVE MODEL #############
+        torch.save(model.state_dict(), os.path.join(STORE_PATH, MODEL_PT_NAME))
+    
+    s = 'MAE/RMSE: | Current Valid: %.2f/%.2f Ep. %d | Best Valid : %.2f/%.2f Ep. %d' % (
+        valid_mae, torch.sqrt(valid_mse), epoch, best_mae, best_rmse, best_epoch)
+    print(s)
+    with open(LOGFILE, 'a') as f:
+        f.write('%s\n' % s)
+    ################################
+
+    s = 'Time elapsed: %.2f min' % ((time.time() - start_time)/60)
+    print(s)
+    with open(LOGFILE, 'a') as f:
+        f.write('%s\n' % s)
+
+
+#####################
+# 07.testing
+######################
+model.eval()
+with torch.set_grad_enabled(False):  # save memory during inference
+
+    train_mae, train_mse = compute_mae_and_mse(model, train_loader,device=DEVICE)
+    valid_mae, valid_mse = compute_mae_and_mse(model, valid_loader,device=DEVICE)
+    test_mae, test_mse = compute_mae_and_mse(model, test_loader,device=DEVICE)
+
+    s = 'MAE/RMSE: | Train: %.2f/%.2f | Valid: %.2f/%.2f | Test: %.2f/%.2f' % (
+        train_mae, torch.sqrt(train_mse),
+        valid_mae, torch.sqrt(valid_mse),
+        test_mae, torch.sqrt(test_mse))
+    print(s)
+    with open(LOGFILE, 'a') as f:
+        f.write('%s\n' % s)
+
+s = 'Total Training Time: %.2f min' % ((time.time() - start_time)/60)
+print(s)
+with open(LOGFILE, 'a') as f:
+    f.write('%s\n' % s)
+
+
+###################
+# 08.save model
+###################
+########## EVALUATE BEST MODEL ######
+model.load_state_dict(torch.load(os.path.join(STORE_PATH, MODEL_PT_NAME)))
+model.eval()
+with torch.set_grad_enabled(False):
+    train_mae, train_mse = compute_mae_and_mse(model, train_loader,device=DEVICE)
+    valid_mae, valid_mse = compute_mae_and_mse(model, valid_loader,device=DEVICE)
+    test_mae, test_mse = compute_mae_and_mse(model, test_loader,device=DEVICE)
+
+    s = 'MAE/RMSE: | Best Train: %.2f/%.2f | Best Valid: %.2f/%.2f | Best Test: %.2f/%.2f' % (
+        train_mae, torch.sqrt(train_mse),
+        valid_mae, torch.sqrt(valid_mse),
+        test_mae, torch.sqrt(test_mse))
+    print(s)
+    with open(LOGFILE, 'a') as f:
+        f.write('%s\n' % s)
+
+########## SAVE PREDICTIONS ######
+all_pred = []
+all_probas = []
+with torch.set_grad_enabled(False):
+    for batch_idx, (features, targets, levels) in enumerate(test_loader):
+        
+        features = features.to(DEVICE)
+        logits, probas = model(features)
+        all_probas.append(probas)
+        predict_levels = probas > 0.5
+        predicted_labels = torch.sum(predict_levels, dim=1)
+        lst = [str(int(i)) for i in predicted_labels]
+        all_pred.extend(lst)
+
+
+torch.save(torch.cat(all_probas).to(torch.device('cpu')), TEST_ALLPROBAS)
+with open(TEST_PREDICTIONS, 'w') as f:
+    all_pred = ','.join(all_pred)
+    f.write(all_pred)
---
+++ full_version/Tutorial.md
@@ -0,0 +1,16 @@
+## STEP0.Setting evns
+- ubuntu evn
+- shin-han-7 github
+- 檢查gpu(cuda=?)
+
+## STEP1.Prepare Data
+- AFAD-Full.tar 解壓縮 to AFAD-Full
+- ROOTDIR = AFAD-Full
+- change train/test/valid csv path in AFAD_train_age.py
+
+## STEP2.Training
+- cd to the path
+- Python AFAD_train_age.py -h
+- epoch= ,batch= ,model name=
+
+## STEP3.Get Model
