578fa1f7658d74a9b41511873308d8a3886b9ce5
shin-han-7 <hannah880708@gmail.com>
2021-01-06 01:23:15 +0800

    update demo/

--- demo/demo_orcnn.py
+++ demo/demo_orcnn.py
@@ -8,8 +8,10 @@ import torch.nn.functional as F
 from torchvision import transforms
 from PIL import Image
 
-pathAdience = "D:/CollageProj/2020_gallary/model/Adience/"
-pathAFAD = "D:/CollageProj/2020_gallary/model/AFAD/"
+AGE_MODEL_PATH = "../model/age/test12072/best_model.pt"
+GENDER_MODEL_PATH = "../model/gender/test1209/model_gen.pt"
+pathAdience = "../model/Adience/"
+pathAFAD = "../model/AFAD/"
 frame_color = (10,10,205)
 ########################
 ### Face
@@ -43,134 +45,47 @@ faceNet = cv.dnn.readNet(faceModel, faceProto)
 ######################
 ### Gender
 ######################
-genderProto = pathAdience+"gender_deploy.prototxt"
-genderModel = pathAdience+"gender_net.caffemodel"
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+from GenCNN import CNN_Model_
 genderList = ['Male', 'Female']
-genderNet = cv.dnn.readNet(genderModel, genderProto)
-faceNet = cv.dnn.readNet(faceModel, faceProto)
 
+def GenderPredict(image_np):
+    image_PIL = Image.fromarray(image_np)
+    custom_transform = transforms.Compose([transforms.Resize((256, 256)),
+                                     transforms.CenterCrop((227,227)),
+                                     transforms.ToTensor()
+                                     ])
+    image = custom_transform(image_PIL)
+    DEVICE = torch.device('cpu')
+    image = image.to(DEVICE)
+    
+    model = CNN_Model_()
+    model.load_state_dict(torch.load(GENDER_MODEL_PATH, map_location=DEVICE))
+    model.eval()
+    image = image.unsqueeze(0)
+
+    with torch.set_grad_enabled(False):
+        output = model(image)
+        probas = output.data.max(dim = 1, keepdim = True)[1]
+        #predict_levels = probas > 0.5
+        #predicted_label = torch.sum(predict_levels, dim=1)
+        #predicted_label = torch.sum(probas, dim=1)
+        #print('Predicted gender:', probas)#predicted_label.item())
+        #genderPredict = int(predicted_label.item())
+        genderLabel = genderList[probas]
+        #print(genderLabel)
+        return genderLabel
 
 ##############################
 ### Age
 ##############################
+from ResNet34 import resnet34_
+
 torch.backends.cudnn.deterministic = True #
-STATE_DICT_PATH = pathAFAD+"model_orcnn.pt"
 GRAYSCALE = False #
 #dataset=afad,start=15,finish=41
-NUM_CLASSES = 26
 ADD_CLASS = 15
+AGE_NUM = 58
 
-
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-#
-class BasicBlock(nn.Module):
-    expansion = 1
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-#
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes, grayscale):
-        self.num_classes = num_classes
-        self.inplanes = 64
-        if grayscale:
-            in_dim = 1
-        else:
-            in_dim = 3
-        super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_dim, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1, padding=2)
-        self.fc = nn.Linear(2048 * block.expansion, (self.num_classes-1)*2)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, (2. / n)**.5)
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        logits = self.fc(x)
-        logits = logits.view(-1, (self.num_classes-1), 2)
-        probas = F.softmax(logits, dim=2)[:, :, 1]
-        return logits, probas
-
-#
-def resnet34(num_classes, grayscale):
-    """Constructs a ResNet-34 model."""
-    model = ResNet(block=BasicBlock, 
-                   layers=[3, 4, 6, 3],
-                   num_classes=num_classes,
-                   grayscale=grayscale)
-    return model
 
 #input<numpy.ndarray>/output age number
 def AgePredict(image_np):
@@ -182,8 +97,8 @@ def AgePredict(image_np):
     DEVICE = torch.device('cpu')
     image = image.to(DEVICE)
     
-    model = resnet34(NUM_CLASSES, GRAYSCALE)
-    model.load_state_dict(torch.load(STATE_DICT_PATH, map_location=DEVICE))
+    model = resnet34_(AGE_NUM , GRAYSCALE)
+    model.load_state_dict(torch.load(AGE_MODEL_PATH, map_location=DEVICE))
     model.eval()
     image = image.unsqueeze(0)
 
@@ -199,8 +114,19 @@ def AgePredict(image_np):
 ###############################
 ### webcam
 ###############################
-cap = cv.VideoCapture(0)
+'''import numpy as np
+photo = 'D:/DeepLearning/GAR/1202/AFAD-Full/19/111/29305-0.jpg'
+img = Image.open(photo)
+img = np.array(img)
+gender = GenderPredict(img)
+age= AgePredict(img)
+print('a/g:',age,gender)'''
+
+
+cap = cv.VideoCapture('D:\DeepLearning\GAR\1223\video\台灣捷運站_3F.mp4')
+fourcc = cv.VideoWriter_fourcc(*'XVID')#fourcc為視訊編解碼器
 padding = 20
+start_time = time.time()
 while cv.waitKey(1) < 0:
     t = time.time()
     hasFrame, frame = cap.read()
@@ -218,10 +144,7 @@ while cv.waitKey(1) < 0:
         face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
 
         ##G
-        blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-        genderNet.setInput(blob)
-        genderPreds = genderNet.forward()
-        gender = genderList[genderPreds[0].argmax()]
+        gender = GenderPredict(face)
 
         ##A
         age=AgePredict(face)
@@ -237,3 +160,4 @@ while cv.waitKey(1) < 0:
     
 cap.release() 
 cv.destroyAllWindows()
+print('RECORD(start)',start_time,'~(finish)',time.time())
